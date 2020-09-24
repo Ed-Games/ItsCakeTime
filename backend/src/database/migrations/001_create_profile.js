80095f23100432383f3b0713b260b37096302031
Ed-Games <edmarcosfilho2015@gmail.com>
2020-09-24 15:11:20 -0300

    update:profile must have only a optnional description

--- backend/src/database/migrations/001_create_profile.js
+++ backend/src/database/migrations/001_create_profile.js
@@ -1,9 +1,7 @@
 exports.up = function(Knex){
     return Knex.schema.createTable('profile', function(table){
         table.increments();
-        table.string('description').notNullable();
-        table.string('email').notNullable();
-        table.string('whatsapp').notNullable();
+        table.string('description');
         table.string('user_id').notNullable();
 
         table.foreign('user_id').references('id').inTable('user').onDelete('CASCADE');
