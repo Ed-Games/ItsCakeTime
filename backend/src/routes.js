bae7f97e9895c51153d98dab505b1c8551d871c3
Ed-Games <edmarcosfilho2015@gmail.com>
2020-09-28 15:40:19 -0300

    create: routes for products

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,6 @@
 const { urlencoded } = require('express')
 const express = require('express')
+const productController = require('./controllers/productController')
 const profileController = require('./controllers/profileController')
 const userController = require('./controllers/userController')
 
@@ -26,5 +27,9 @@ routes.delete('/profile/delete/:id',profileController.delete) //NOT TO USE IN PR
 routes.get('/profile', profileController.index) //NOT TO USE IN PRODUCTION
 
 routes.put('/profile/update/:id', profileController.update)
+
+routes.get('/products', productController.index)
+
+routes.post('/products/create', productController.create)
 
 module.exports = routes
