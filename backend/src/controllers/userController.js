e84564da87877d698553428269b78cff649cf380
Ed-Games <edmarcosfilho2015@gmail.com>
2020-09-28 14:41:32 -0300

    update: creating or deleting a profile with its user

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,5 +1,6 @@
 const { query } = require('express')
 const connection = require('../database/connection')
+const profileController = require('./profileController')
 
 module.exports = {
     async index(request, response){
@@ -16,17 +17,27 @@ module.exports = {
             userName,
             email,
             password,
-            whatsapp
         })
 
+        id = await connection('user').select('user.id').where('user.email', "=", email)
+        console.log("aqui se encontra o id necessario", id)
+
+        user_id = id[0]['id']
+        
+        await connection('profile').insert({
+            user_id,
+            whatsapp
+        })
 
         return response.status(201).json("created user")
     },
 
     async delete(request,response){
         const {id} = request.params
-        const userToDelete = await connection('user').select('*').where("user.id","=",id).delete();
-        return response.json(userToDelete)
+        //const userToDelete = await connection('user').select('*').where("user.id","=",id).delete();
+        const userToDelete = await connection('user').select('*').where("user.id","=",id).delete()
+        const profileToDelete = await connection('profile').select("*").where('profile.user_id', "=", id).delete()
+        return response.json([userToDelete, profileToDelete])
     },
 
     async resetPassword(request,response){
