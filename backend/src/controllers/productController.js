f67feb591f5844e91090b6a9a2785423fb51fcab
Ed-Games <edmarcosfilho2015@gmail.com>
2020-09-29 18:29:08 -0300

    create: filter and detail methods

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -19,5 +19,32 @@ module.exports = {
         })
 
         return response.status(201).json("product created");
+    },
+
+    async detail(request,response){
+        const {id} = request.params
+        const product = await connection('product').select('*').where('product.id','=',id)
+        return response.json(product)
+    },
+
+    /*async filter(request,response){
+        const{price, userName, category} = request.body
+        const {product, profile}=await connection('product')
+        .join('profile','profile.user_id','=','product.user_id')
+        .select('*').where()
+        const products = await connection('product').select('*');
+        return response.json(products)
+        
+    },*/
+
+    async filter(request,response){
+        const {price, category, userName} = request.body
+        const products = await connection('product')
+        .join('user','user.id','=',"product.user_id")
+        .select('*')
+        .where("user.userName","=", userName)
+        .andWhere("product.price","=",price)
+        .andWhere("product.category","=",category)
+        return response.json(products)
     }
 }
