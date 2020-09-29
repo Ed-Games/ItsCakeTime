38f26f9c46887ffe5e3384fd8506e3564507eef7
Ed-Games <edmarcosfilho2015@gmail.com>
2020-09-29 19:28:38 -0300

    update: delete product route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -39,4 +39,6 @@ routes.get('/products/:id/', productController.detail)
 
 routes.get('/product/filter/',productController.filter)
 
+routes.delete('/products/delete/:id',productController.delete)
+
 module.exports = routes
