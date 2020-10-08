5e7323d1e29b7e49e59ba65a92d5d6aea111ed74
Ed-Games <edmarcosfilho2015@gmail.com>
2020-10-08 13:44:26 -0300

    create: functions for generate and refresh tokens

--- backend/src/services/authorization.js
+++ backend/src/services/authorization.js
@@ -1,8 +1,33 @@
+const { response } = require('express')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 
 let User = null
+
+const refreshTokens = []
+
+function generateAccessToken(userName){
+    const userauth = {name : userName}
+    const accessToken = jwt.sign(userauth, ''+process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30s'})
+    console.log(userauth)
+    return accessToken
+}
+
+function refreshToken(request,response){
+    const refreshToken = request.body.token
+    console.log(String(refreshToken),refreshTokens)
+    if (refreshToken == null) return response.sendStatus(401)
+
+    if (!refreshTokens.includes(String(refreshToken))) return response.sendStatus(403)
+
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (error, user)=>{
+        if(error) return response.sendStatus(403)
+        const accessToken = generateAccessToken({name : user.name})
+
+        return response.json(accessToken)
+    })
+}
 
 function authenticateToken(request,response, next){
     const authHeader = request.headers['authorization']
@@ -27,6 +52,9 @@ function GetRequestUser(){
 
 module.exports = {
     authenticateToken,
-    GetRequestUser
+    GetRequestUser,
+    refreshTokens,
+    generateAccessToken,
+    refreshToken
 
 }
