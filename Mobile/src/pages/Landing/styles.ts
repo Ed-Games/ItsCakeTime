c7b637d4d4178051501268628a66e2134c9dc924
Ed-Games <edmarcosfilho2015@gmail.com>
2020-10-26 15:47:49 -0300

    create: landing styles

---
+++ Mobile/src/pages/Landing/styles.ts
@@ -0,0 +1,70 @@
+const styles = {
+    container : {
+        backgroundColor: '#9553A0',
+        flex: 1,
+        textAlign: 'center',
+        alignItems: 'center',
+
+    },
+
+    title: {
+        marginTop: 20,
+        marginBottom: 10,
+        fontSize: 20,
+        fontFamily: 'Archivo600_SemiBold',
+        color: '#FFF'
+        
+    },
+
+    logo : {
+        marginBottom: 10
+    },
+
+    contentText : {
+        fontFamily: 'Poppins_500Medium_Italic',
+        color: '#FFF',
+        fontSize: 16,
+        textAlign: 'center',
+        marginTop: 10
+    },
+
+    actionText : {
+        fontFamily: 'Poppins_500Medium_Italic',
+        color: '#FFF',
+        fontSize: 20,
+        textAlign: 'center',
+        marginTop: 20
+    },
+
+    buttonsView : {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+
+    clientButton: {
+        backgroundColor: '#F783EC',
+        borderRadius: 10,
+        width: 133,
+        height: 62,
+        marginRight: 10,
+        marginTop: 50
+    },
+
+    ButtonText : {
+        fontFamily: 'Poppins_500Medium_Italic',
+        color: '#FFF',
+        fontSize: 18,
+        textAlign: 'center',
+    },
+
+    bakerButton: {
+        backgroundColor: '#BC71CF',
+        borderRadius: 10,
+        width: 133,
+        height: 62,
+        marginLeft: 10,
+        marginTop: 50
+    },
+}
+
+export default styles
