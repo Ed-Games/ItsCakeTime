ee6580a65972bafab278eea1765c654a49430f9e
Ed-Games <edmarcosfilho2015@gmail.com>
2020-10-26 15:47:39 -0300

    create: landing page

---
+++ Mobile/src/pages/Landing/Landing.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Image, Text, View } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+import bakerImage from '../../images/baker.png'
+import Logo from '../../images/logo.png'
+import styles from './styles'
+
+
+export default function Landing(){
+    return(
+        <View style={styles.container} >
+            <Text style={styles.title} >Seja bem vindo ao</Text>
+            <Image style={styles.logo} source={Logo} />
+            <Image source={bakerImage}  />
+            <Text style={styles.contentText}>Ter uma padaria por perto é bom, mas agora ela esta no seu bolso</Text>
+            <Text style={styles.actionText}>O que deseja fazer?</Text>
+            <View style={styles.buttonsView}>
+                <RectButton style={styles.clientButton}><Text style={styles.ButtonText}>Fazer uma encomenda</Text></RectButton>
+                <RectButton style={styles.bakerButton}><Text style={styles.ButtonText}>Ver minha confeitaria</Text></RectButton>
+            </View>
+        </View>
+    )
+}
