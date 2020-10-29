bd5806b70ef9347db93faddacfcbf7d0f09adac0
Ed-Games <edmarcosfilho2015@gmail.com>
2020-10-29 15:25:02 -0300

    remove: repited code | add: components

--- Mobile/src/pages/ProductsList/ProductsList.tsx
+++ Mobile/src/pages/ProductsList/ProductsList.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react'
-import { Text, View,TextInput, Image, ImageBackground} from 'react-native'
+import { Text, View,TextInput, Image, ImageBackground, ScrollView} from 'react-native'
 import {Picker} from '@react-native-community/picker'
 import styles from './styles'
 import {Feather, FontAwesome} from '@expo/vector-icons'
 
 import selectImg from '../../images/select.png'
 import background from '../../images/background.png'
-import Cake from '../../images/cake.jpg'
 import { RectButton } from 'react-native-gesture-handler'
+import ProductItem from '../../components/ProductItem/ProductItem'
 
 export default function ProductsList() {
     const [value, setValue] = useState("0")
@@ -45,33 +45,16 @@ export default function ProductsList() {
                 </RectButton>
 
                 <ImageBackground source={background} style={styles.ProductsList}>
-                    <View style={styles.ProductItem}>
-                        <View style={styles.FlexRowView}>
-                            <Image style={styles.ProductImage} source={Cake} />
-                            <View style={styles.FlexColumnView}>
-                                <Text style={styles.CategoryText}>#Bolos</Text>
-                                <Text style={styles.ProductTitle}>Bolo de aniversario</Text>
-                                <View style={styles.FlexRowView}>
-                                    <Text style={styles.ProductPriceLabel}>Preço: </Text><Text style={styles.ProductPriceValue}>R$ 35,00</Text>
-                                </View>
-                            </View>
-                        </View>
-                        <Text style={styles.ProductDescription}>Bolo de creme com calda de morango, perfeito para aniversarios infantis. Entregas ocorrem em até um dia após o pedido.</Text>
-                        <View style={styles.FlexRowView}>
-                            <RectButton style={styles.WhatsappButton}>
-                                <View style={styles.FlexRowView}>
-                                    <FontAwesome name="whatsapp" size={24} color='#FFF' style={{marginLeft: 5, marginTop: 5}} />
-                                    <Text style={styles.ButtonText}>Whatsapp</Text>
-                                </View>
-                            </RectButton>
-                            <RectButton style={styles.EmailButton}>
-                                <View style={styles.FlexRowView}>
-                                    <Feather name="mail" size={24} color='#FFF' style={{marginLeft: 5, marginTop: 5}} />
-                                    <Text style={styles.ButtonText}>Enviar um Email</Text>
-                                </View>
-                            </RectButton>
-                        </View>
-                        </View>
+                    <ScrollView contentContainerStyle={{
+                        alignItems: 'center',
+                        paddingTop: 20,
+                        paddingBottom:60
+                    }}>
+                        <ProductItem />
+                        <ProductItem />
+                        <ProductItem />
+                        <ProductItem />
+                    </ScrollView>
                 </ImageBackground>
 
         </View>
