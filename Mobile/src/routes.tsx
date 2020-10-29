bd809a546952a88b53635679cc29c398c267d2db
Ed-Games <edmarcosfilho2015@gmail.com>
2020-10-29 13:51:58 -0300

    create: ProductList Route

--- Mobile/src/routes.tsx
+++ Mobile/src/routes.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {NavigationContainer} from '@react-navigation/native'
 import {createStackNavigator} from '@react-navigation/stack'
 import Landing from './pages/Landing/Landing'
+import ProductsList from './pages/ProductsList/ProductsList'
 
 const {Navigator, Screen} = createStackNavigator()
 
@@ -12,6 +13,7 @@ export default function Routes() {
                 headerShown: false
             }}>
                 <Screen name="Landing" component={Landing} />
+                <Screen name="ProductsList" component={ProductsList} />
             </Navigator>
         </NavigationContainer>
     )
