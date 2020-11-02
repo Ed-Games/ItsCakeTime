8079bba2a3a47ef3cd1c46f6ff04e2d46fe2d466
Ed-Games <edmarcosfilho2015@gmail.com>
2020-11-02 14:22:04 -0300

    create: Profile page

---
+++ Mobile/src/pages/Profile/Profile.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { ImageBackground, Text, View } from 'react-native'
+import Header from '../../components/Header/Header'
+import Waves from '../../images/waves.png'
+import styles from './style'
+
+import Avatar from '../../images/avatar.png'
+import WhatsappButton from '../../components/WhatsappButton/WhatsappButton'
+import EmailButton from '../../components/EmailButton/EmailButton'
+import { RectButton } from 'react-native-gesture-handler'
+import { FontAwesome } from '@expo/vector-icons'
+
+export default function Profile() {
+    return(
+        <View style={styles.container}>
+            <View>
+                <ImageBackground style={styles.Waves} source={Waves}>
+                    <View style={styles.FlexRowView}>
+                        <Header image={Avatar} />
+                    </View>
+                        <Text style={styles.Name}>Alice Andrade Campus</Text>
+                </ImageBackground>
+            </View>
+            <View style={styles.InfoView}>
+                <Text style={styles.TopicText}>Biografia:</Text>
+                <Text style={styles.ContentText}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Ut at varius dolor. Curabitur dignissim, neque vitae condimentum 
+                accumsan, lacus massa ultricies augue, vel tincidunt eros quam non 
+                sapien. Duis feugiat.
+                </Text>
+                <Text style={styles.TopicText}>Especialidades:</Text>
+                <Text style={styles.ContentText}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Ut at varius dolor. Curabitur dignissim, 
+                </Text>
+                <Text style={styles.TopicText}>Contato:</Text>
+                <View style={styles.ButtonsView}>
+                    <WhatsappButton />
+                    <EmailButton />
+                </View>
+                <View style={{alignItems:'center'}}>
+                <RectButton style={styles.ListButton} >
+                    <View style={styles.FlexRowView}>
+                        <FontAwesome name="birthday-cake" size={24} color='#FFF' style={{marginLeft: 5, marginTop: 5}} />
+                        <Text style={styles.ButtonText}>Lista de produtos</Text>
+                    </View>
+                </RectButton>
+                </View>
+            </View>
+        </View>
+    )
+}
