1e38063979ed6fc48dae1f6c08a1e8c4120ba1a3
Ed-Games <edmarcosfilho2015@gmail.com>
2020-11-02 14:47:51 -0300

    create: Profile Product Route

--- Mobile/src/routes.tsx
+++ Mobile/src/routes.tsx
@@ -4,6 +4,7 @@ import {createStackNavigator} from '@react-navigation/stack'
 import Landing from './pages/Landing/Landing'
 import ProductsList from './pages/ProductsList/ProductsList'
 import Profile from './pages/Profile/Profile'
+import ProfileProducts from './pages/ProfileProducts/ProfileProducts'
 
 const {Navigator, Screen} = createStackNavigator()
 
@@ -16,6 +17,7 @@ export default function Routes() {
                 <Screen name="Landing" component={Landing} />
                 <Screen name="ProductsList" component={ProductsList} />
                 <Screen name="Profile" component={Profile} />
+                <Screen name="ProfileProducts" component={ProfileProducts} />
             </Navigator>
         </NavigationContainer>
     )
