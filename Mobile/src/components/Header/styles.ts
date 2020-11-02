a68b510a096b5014cc6d11a35d66a98b3ae85295
Ed-Games <edmarcosfilho2015@gmail.com>
2020-11-02 14:19:06 -0300

    create: header styles

---
+++ Mobile/src/components/Header/styles.ts
@@ -0,0 +1,33 @@
+const styles = {
+    title:{
+        fontSize: 20,
+        fontFamily: 'Archivo600_SemiBold',
+        color: '#FFF',
+        marginLeft: 50,
+        marginRight: 50
+    },
+
+    iconButton: {
+        width: 27,
+        height: 24,
+        borderRadius: 5,
+        alignItems: 'center',
+    },
+
+    FlexRowView : {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 67
+    },
+
+    Avatar : {
+        marginLeft:101,
+        marginRight: 98,
+        marginTop: 18,
+        width:96,
+        height:96,
+        borderRadius: 50
+    }
+}
+
+export default styles
