74ef3e676050823b8213711ad88307ede6b51d17
Ed-Games <edmarcosfilho2015@gmail.com>
2020-11-02 14:18:56 -0300

    create: header component

---
+++ Mobile/src/components/Header/Header.tsx
@@ -0,0 +1,35 @@
+import { Feather } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+import React from 'react'
+import { Image, Text, View } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+import styles from './styles'
+
+interface Headerprops {
+    title?: String,
+    image?: String
+}
+
+export default function Header(props:Headerprops) {
+
+    const navigation = useNavigation()
+
+    function handleNavigateToPreviousPage(){
+        navigation.goBack()
+    }
+
+    return(
+        <View style={styles.FlexRowView}>
+            <RectButton onPress={handleNavigateToPreviousPage} style={styles.iconButton}>
+                <Feather  name="arrow-left" size={24} color='#FFF' />
+            </RectButton>
+            {props.title && <Text style={styles.title}>Produtos Disponíveis:</Text> }
+            {props.image && 
+                <Image style={styles.Avatar} source={props.image} />
+            }
+            <RectButton>
+                <Feather  style={styles.iconButton} name="menu" size={24} color='#FFF' />
+            </RectButton>
+        </View>
+    )
+}
