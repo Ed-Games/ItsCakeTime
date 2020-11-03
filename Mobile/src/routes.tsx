d0e5bad344325e8b489dcd05f9251163e95435bc
Ed-Games <edmarcosfilho2015@gmail.com>
2020-11-03 10:21:53 -0300

    delete: Stack navigation screens | create: Drawer navigation screens

--- Mobile/src/routes.tsx
+++ Mobile/src/routes.tsx
@@ -1,24 +1,33 @@
 import React from 'react'
 import {NavigationContainer} from '@react-navigation/native'
 import {createStackNavigator} from '@react-navigation/stack'
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Landing from './pages/Landing/Landing'
 import ProductsList from './pages/ProductsList/ProductsList'
 import Profile from './pages/Profile/Profile'
-import ProfileProducts from './pages/ProfileProducts/ProfileProducts'
+
 
 const {Navigator, Screen} = createStackNavigator()
+const Drawer = createDrawerNavigator()
 
 export default function Routes() {
     return(
         <NavigationContainer>
-            <Navigator screenOptions={{
+            {/*<Navigator screenOptions={{
                 headerShown: false
             }}>
                 <Screen name="Landing" component={Landing} />
                 <Screen name="ProductsList" component={ProductsList} />
                 <Screen name="Profile" component={Profile} />
-                <Screen name="ProfileProducts" component={ProfileProducts} />
-            </Navigator>
+                <Screen name="ProfileProducts" component={Profile} />
+            </Navigator>*/}
+            <Drawer.Navigator initialRouteName="Landing">
+                <Drawer.Screen name="Landing" component={Landing} />
+                <Drawer.Screen name="ProductsList" component={ProductsList} />
+                <Drawer.Screen      
+                name="Profile" component={Profile} />
+                <Drawer.Screen name="ProfileProducts" component={Profile} />
+            </Drawer.Navigator>
         </NavigationContainer>
     )
 }
