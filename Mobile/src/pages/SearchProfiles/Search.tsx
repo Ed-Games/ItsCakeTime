ba56b32d7bf0bd213971a338271d2c5a2195e26d
Ed-Games <edmarcosfilho2015@gmail.com>
2020-11-04 18:06:22 -0300

    create: search page

--- Mobile/src/pages/SearchProfiles/Search.tsx
+++ Mobile/src/pages/SearchProfiles/Search.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { ImageBackground, Text, View } from 'react-native'
+
+import styles from './styles'
+import Waves from '../../images/waves.png'
+import Header from '../../components/Header/Header'
+import { RectButton, TextInput } from 'react-native-gesture-handler'
+import { Feather } from '@expo/vector-icons'
 
 export default function Search(){
     return(
-        <Text>Search</Text>
+        <View style={styles.container}>
+            <View>
+                <ImageBackground source={Waves} style={styles.Waves}>
+                    <Header title="Padeiros Disponíveis: "/>
+                    <View style={styles.search}>
+                        <TextInput placeholder="Procure um confeiteiro pelo nome de perfil" />
+                        <RectButton>
+                            <Feather name="search" size={24} color='#FFF'/>
+                        </RectButton>
+                    </View>
+                </ImageBackground>
+            </View>
+        </View>
     )
 }
 
--- Mobile/src/pages/SearchProfiles/styles.ts
+++ Mobile/src/pages/SearchProfiles/styles.ts
@@ -1,5 +1,27 @@
-const styles = {
+import { Dimensions } from "react-native"
 
+const styles = {
+    container : {
+        flex: 1,
+        backgroundColor: '#F5F5F5',
+        alignItems: 'center',
+    },
+
+    Waves : {
+        width: Dimensions.get('screen').width,
+        height:300
+    },
+
+    search : {
+        width: 315,
+        height:40,
+        backgroundColor: '#FFF',
+        borderRadius: 10,
+        marginLeft: 22,
+        marginRight: 22,
+        marginTop: 35,
+        flexDirection: 'row',
+    }
 }
 
 export default styles
