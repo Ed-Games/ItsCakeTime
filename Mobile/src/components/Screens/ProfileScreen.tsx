a8974033664b2a81fbe29f338c34a9420dcf321f
Ed-Games <edmarcosfilho2015@gmail.com>
2020-11-06 14:17:43 -0300

    create: ProfileScreen component

---
+++ Mobile/src/components/Screens/ProfileScreen.tsx
@@ -0,0 +1,54 @@
+import { createStackNavigator } from '@react-navigation/stack'
+import React from 'react'
+import { Feather } from '@expo/vector-icons';
+import Profile from '../../pages/Profile/Profile';
+
+
+const Stack = createStackNavigator()
+
+export default function ProductScreen({navigation}) {
+
+    function handleNavigateToPreviousPage(){
+        navigation.goBack()
+    }
+
+    return(
+        <Stack.Navigator>
+             <Stack.Screen name="Profile" component={Profile} options={{
+                headerStyle: header,
+                headerTitleStyle: title,
+                title: '',
+                headerRight: () => (
+                    <Feather
+                    name="menu"
+                    size={25}
+                    color="#FFF"
+                    onPress={() => navigation.openDrawer()}
+                  />
+                ),
+                headerLeft: ()=> (
+                    <Feather  
+                    name="arrow-left" 
+                    size={24} 
+                    color='#FFF'
+                    onPress={handleNavigateToPreviousPage} />
+                )
+          }} />
+        </Stack.Navigator>
+    )
+}
+
+const header = {
+    backgroundColor: '#9553A0',
+    elevation: 0,
+    shadowOpacity: 0,
+}
+
+const title = {
+    fontSize: 20,
+    fontFamily: 'Archivo600_SemiBold',
+    color: '#FFF',
+    marginLeft: 14,
+    marginRight: 50,
+    width: 315,
+}
