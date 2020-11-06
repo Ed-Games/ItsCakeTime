554eea921fb8f289c9909865a88807079d3c9b1f
Ed-Games <edmarcosfilho2015@gmail.com>
2020-11-06 13:49:06 -0300

    create: DrawerContent styles

---
+++ Mobile/src/components/DrawerContent/styles.ts
@@ -0,0 +1,28 @@
+import { Dimensions, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container : {
+        backgroundColor: '#F5F5F5',
+        alignItems: 'center',
+        flex: 1,
+    },
+
+    ButtonLink:{
+        marginTop: 30,
+    },
+
+    LinkText : {
+        color: '#BA68C8',
+        fontSize: 16,
+        fontFamily: 'Poppins_600SemiBold'
+    },
+
+    LineSpace : {
+        borderColor: '#BA68C8',
+        height: 1,
+        width: 224,
+        backgroundColor: '#BA68C8'
+    }
+})
+
+export default styles
