a622d183be3735b5ccfda39e7cc83d1f52d90b47
Ed-Games <edmarcosfilho2015@gmail.com>
2020-11-07 14:08:19 -0300

    update: navigation and components done

--- Mobile/src/pages/SearchProfiles/Search.tsx
+++ Mobile/src/pages/SearchProfiles/Search.tsx
@@ -1,25 +1,53 @@
 import React from 'react'
-import { ImageBackground, Text, View } from 'react-native'
+import { Image, ImageBackground, Text, View } from 'react-native'
 
 import styles from './styles'
 import Waves from '../../images/waves.png'
 import Header from '../../components/Header/Header'
-import { RectButton, TextInput } from 'react-native-gesture-handler'
+import { RectButton, ScrollView, TextInput } from 'react-native-gesture-handler'
 import { Feather } from '@expo/vector-icons'
+import Avatar from '../../images/avatar.png'
+import { useNavigation } from '@react-navigation/native'
 
 export default function Search(){
+
+    const navigation = useNavigation()
+
+    function handleNavigateToProfile(){
+        navigation.navigate('Profile')
+    }
+
     return(
         <View style={styles.container}>
             <View>
                 <ImageBackground source={Waves} style={styles.Waves}>
-                    <Header title="Padeiros Disponíveis: "/>
                     <View style={styles.search}>
-                        <TextInput placeholder="Procure um confeiteiro pelo nome de perfil" />
-                        <RectButton>
-                            <Feather name="search" size={24} color='#FFF'/>
-                        </RectButton>
+                        <TextInput style={styles.searchInput} placeholder="Procure um confeiteiro pelo nome de perfil" />
+                        <View style={styles.searchButtonView}>
+                            <RectButton style={styles.SearchButton}>
+                                <Feather name="search" size={24} color='#FFF'/>
+                            </RectButton>
+                        </View>
                     </View>
                 </ImageBackground>
+
+                <ScrollView>
+                <RectButton onPress={handleNavigateToProfile}>
+                    <View style={styles.AvatarView}>
+                        <Image style={styles.Avatar} source={Avatar} />
+                        <View>
+                            <Text style={styles.avatarname}>Alice Andrade Campus</Text>
+                            <View style={{flexDirection:'row'}}>
+                            <Text style={styles.avatarSpecialtyBold}>Especialidades: </Text><Text style={styles.avatarSpecialty}>Bolos e tortas para eventos</Text>
+                            </View>
+                        </View>
+                    </View>
+                </RectButton>
+                
+
+                
+                </ScrollView>
+                
             </View>
         </View>
     )
