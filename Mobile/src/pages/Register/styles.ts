744eaa29053662117b2f5c70faeacf36049301a8
Ed-Games <edmarcosfilho2015@gmail.com>
2020-11-09 14:48:49 -0300

    create: Register Styles

---
+++ Mobile/src/pages/Register/styles.ts
@@ -0,0 +1,67 @@
+import { Dimensions, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container : {
+        backgroundColor: '#F5F5F5',
+        flex: 1,
+        textAlign: 'center',
+        alignItems: 'center',
+    },
+    waves : {
+        width: Dimensions.get('screen').width,
+        flex: 1,
+        height: 210,
+        alignItems: 'center',
+    },
+
+    FormView:{
+        backgroundColor: '#F5F5F5',
+        width: Dimensions.get('screen').width - 136,
+        height: 536,
+        alignItems: 'center',
+
+    },
+    FormInputView:{
+        width: Dimensions.get('screen').width -120,
+        height: 45,
+        backgroundColor: '#E6E6E6',
+        borderRadius: 10,
+        justifyContent: 'center',
+    },
+
+    Input:{
+        color: '#9E9999',
+        fontFamily: 'Poppins_500Medium',
+        fontSize: 15,
+        marginTop:10,
+        marginLeft:10,
+        marginBottom:9,
+    },
+
+    InputText:{
+        color: '#455A64',
+        fontFamily: 'Poppins_500Medium',
+        fontSize: 15,
+        marginBottom: 4,
+        alignSelf: 'flex-start'
+
+    },
+
+    submitButton:{
+        backgroundColor: '#9553A0',
+        width:Dimensions.get('screen').width -120,
+        height:40,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 10
+    },
+
+    submitButtonText:{
+        color: '#FFF',
+        fontFamily: 'Poppins_500Medium',
+        fontSize: 15,
+    }
+})
+
+export default styles
