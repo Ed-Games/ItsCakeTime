ba7f2528d87bfa344271e7fdd45d2e43a6c53285
Ed-Games <edmarcosfilho2015@gmail.com>
2020-11-09 14:48:39 -0300

    create: Register Screen

---
+++ Mobile/src/pages/Register/Register.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Image, ImageBackground, Text, View, Dimensions } from 'react-native'
+import Waves from '../../images/waves.png'
+import styles from './styles'
+import Header from '../../components/Header/Header'
+import { RectButton, ScrollView, TextInput } from 'react-native-gesture-handler'
+import Input from '../../components/Input/Input'
+
+export default function Register(){
+    return(
+        <View style={styles.container}>
+            <View style={{height:230}}>
+                <ImageBackground source={Waves} style={styles.waves}>
+                    <Header title="Cadastre-se para começar a vender" />
+                </ImageBackground>
+            </View>
+            <View>
+                <View style={styles.FormView}>
+                   <ScrollView 
+                   style={{
+                       width:Dimensions.get('screen').width,
+                       
+                   }}
+
+                   contentContainerStyle={{
+                       alignItems: 'center',
+                       paddingBottom: 80
+                   }}
+                   >
+                    <Input name="Nome: " placeholder="Seu Nome de usuário" />
+                    <Input name="E-mail: " placeholder="Informe seu e-mail" />
+                    <Input name="Senha: " placeholder="Informe uma boa senha" />
+                    <Input name="Confirmar senha: " placeholder="Repita a senha anterior " />
+                    <Input name="Whatsapp: " placeholder="Seu numero de whatsapp" />
+                    <RectButton style={styles.submitButton}>
+                        <Text style={styles.submitButtonText}>Finalizar Cadastro</Text>
+                    </RectButton>
+                    
+                   </ScrollView>
+                </View>
+            </View>
+            
+        </View>
+    )
+}
