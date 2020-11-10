58401e16869617f8a237b6ce0422408f69377a2d
Ed-Games <edmarcosfilho2015@gmail.com>
2020-11-10 10:09:24 -0300

    change: header style

--- Mobile/src/components/Header/styles.ts
+++ Mobile/src/components/Header/styles.ts
@@ -5,32 +5,22 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontFamily: 'Archivo600SemiBold',
         color: '#FFF',
-        width: Dimensions.get('screen').width -30
+        
     },
 
     iconButton: {
         width: 27,
         height: 24,
         borderRadius: 5,
-        alignItems: 'center',
-        marginBottom: 70
+        alignSelf: 'flex-start',
+        marginBottom: 30
     },
 
-    FlexRowView : {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginTop: 67,
+    headerView:{
+        width: Dimensions.get('screen').width,
+        height:80,
         alignItems: 'center',
-        textAlign: 'center',
-    },
-
-    Avatar : {
-        marginLeft:101,
-        marginRight: 98,
-        marginTop: 18,
-        width:96,
-        height:96,
-        borderRadius: 50
+        marginTop: 30
     }
 })
 
