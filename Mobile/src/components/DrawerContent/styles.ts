4645fad8d15d444bd7b9d7b6e5e38296540081af
Ed-Games <edmarcosfilho2015@gmail.com>
2020-11-11 14:38:38 -0300

    update: DrawerContent Styles

--- Mobile/src/components/DrawerContent/styles.ts
+++ Mobile/src/components/DrawerContent/styles.ts
@@ -8,20 +8,69 @@ const styles = StyleSheet.create({
     },
 
     ButtonLink:{
-        marginTop: 30,
+        marginTop: 20,
+        marginBottom: 20,
+        height: 24,
+        justifyContent: 'center',
+        alignSelf: 'flex-start',
+        flexDirection: 'row',
     },
 
     LinkText : {
-        color: '#BA68C8',
+        color: '#9330AC',
         fontSize: 16,
-        fontFamily: 'Poppins_600SemiBold'
+        fontFamily: 'Poppins_600SemiBold',
+        marginLeft: 10
     },
 
     LineSpace : {
-        borderColor: '#BA68C8',
+        borderColor: '#9330AC',
         height: 1,
         width: 224,
-        backgroundColor: '#BA68C8'
+        backgroundColor: '#9330AC'
+    },
+
+    BottomContainer:{
+        flex: 1,
+        justifyContent: 'flex-end',
+        width: 255
+    },
+
+    SignOutButton:{
+        flexDirection: 'row',
+        marginLeft: 25,
+        marginBottom: 25
+    },
+
+    SignOutText: {
+        fontFamily: 'Poppins_600SemiBold',
+        fontSize: 16,
+        lineHeight: 24,
+        color: '#9330AC',
+        marginLeft: 10
+    },
+
+    ImageView: {
+        height: 81,
+        marginTop: 30,
+        marginBottom: -50,
+        flexDirection: 'row',
+    },
+
+    Avatar:{
+        width:81,
+        height:81, 
+        borderRadius: 50
+    },
+
+    AvatarNameText:{
+        alignSelf: 'center',
+        fontFamily: 'Poppins_600SemiBold',
+        fontSize: 16,
+        lineHeight:24,
+        width:139,
+        marginLeft:10,
+        color: '#9330AC',
     }
 })
 
