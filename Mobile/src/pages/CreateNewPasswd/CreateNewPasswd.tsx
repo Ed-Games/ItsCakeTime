34d42aa00dfa81f79df5055b889557ae67e2643e
Ed-Games <edmarcosfilho2015@gmail.com>
2020-11-12 13:40:44 -0300

    create: CreateNewPasswd page

---
+++ Mobile/src/pages/CreateNewPasswd/CreateNewPasswd.tsx
@@ -0,0 +1,30 @@
+import { useNavigation } from '@react-navigation/native'
+import React from 'react'
+import { Image, Text, View } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+import Header from '../../components/Header/Header'
+import Input from '../../components/Input/Input'
+import Security from '../../images/Security.png'
+import styles from './styles'
+
+export default function CreateNewPasswd(){
+
+    const navigation = useNavigation()
+
+    function handleNavigateToProfile(){
+        navigation.navigate('Profile')
+    }
+
+    return(
+        <View style={styles.container}>
+        <Header title="Digite uma nova senha segura" />
+        <Image style={styles.Image} source={Security} />
+        <Input name="Nova senha :" placeholder="Nova senha" titleMode="Light" />
+        <Input name="Confirmar senha" placeholder="Confirme a nova senha" titleMode='Light' />
+        <RectButton onPress={handleNavigateToProfile} style={styles.submitButton}>
+            <Text style={styles.submitButtonText}>Salvar</Text>
+        </RectButton>
+
+    </View>
+    )
+}
