06f7abdada51a751d35f1229ff6de2c1cf7e97c5
Ed-Games <edmarcosfilho2015@gmail.com>
2020-11-13 15:10:24 -0300

    update: options atribute was created

--- Mobile/src/components/Input/Input.tsx
+++ Mobile/src/components/Input/Input.tsx
@@ -4,21 +4,25 @@ import styles from './styles'
 
 interface InputProps{
     name: string,
-    placeholder: string,
-    titleMode?: string
+    placeholder?: string,
+    options?: {
+        titleMode?: string,
+        customStyle?: object,
+        useAsTextArea?: boolean
+    }
 }
 
 export default function Input(props: InputProps){
     return(
         <>
-            {props.titleMode =='Light'? 
+            {props.options?.titleMode =='Light'? 
             (
-                <Text style={[styles.InputText, {color:"#FFF"}]}>{props.name}</Text>
+                <Text style={[styles.InputText,{color:"#FFF"}]}>{props.name}</Text>
             ):(
                 <Text style={styles.InputText}>{props.name}</Text>
             )}
-                <View style={styles.FormInputView}>
-                    <TextInput style={styles.Input} placeholder={props.placeholder}>
+                <View style={[styles.FormInputView,props.options?.customStyle]}>
+                    <TextInput multiline={props.options?.useAsTextArea} style={styles.Input} placeholder={props.placeholder}>
                     </TextInput>
                 </View>
         </>
