e76e689e9769fe8fca9034dd3368e5e1ca97fb45
Ed-Games <edmarcosfilho2015@gmail.com>
2020-11-13 15:09:59 -0300

    change: styles for header

--- Mobile/src/components/Header/styles.ts
+++ Mobile/src/components/Header/styles.ts
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
         width: Dimensions.get('screen').width,
         height:80,
         alignItems: 'center',
-        marginTop: 30
+        marginTop: 30,
     }
 })
 
