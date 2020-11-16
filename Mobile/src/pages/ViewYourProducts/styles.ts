26ef7a78befc54e9d32c0432284f55e7e9366b71
Ed-Games <edmarcosfilho2015@gmail.com>
2020-11-16 12:56:51 -0300

    add: styles for plus button

--- Mobile/src/pages/ViewYourProducts/styles.ts
+++ Mobile/src/pages/ViewYourProducts/styles.ts
@@ -20,6 +20,28 @@ const styles = StyleSheet.create({
         marginLeft:10,
         alignItems: 'center',
     },
+
+    plusButton:{
+        marginTop: -300,
+        backgroundColor: '#9553A0',
+        alignSelf: 'flex-end',
+        width: 60,
+        height: 60,
+        borderRadius: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginRight:10,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 5,
+        },
+        shadowOpacity: 0.36,
+        shadowRadius: 6.68,
+
+        elevation: 11,
+
+    }
 })
 
 export default styles
