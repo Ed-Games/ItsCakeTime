d838f7547954c83cb2e380bdd4d6cf153f445f63
Ed-Games <edmarcosfilho2015@gmail.com>
2020-12-01 07:02:51 -0300

    update: add auth configs to api object

--- Mobile/src/services/api.ts
+++ Mobile/src/services/api.ts
@@ -1,7 +1,30 @@
 import axios from 'axios'
+import GetUser from '../utils/GetUser'
+
 
 const api = axios.create({
     baseURL: 'http://10.0.0.103:3333'
 })
+
+api.interceptors.request.use(
+    config => {
+        return GetUser().then(user => {
+            if(user){
+                const userData = JSON.parse(user);
+                if(userData || userData.accessToken){
+                    config.headers.authorization = `Bearer ${userData.accessToken}`
+                    return Promise.resolve(config)
+                }
+            }
+        }).catch(error => {
+            console.log(error)
+            return Promise.resolve(config)
+          })
+    },
+    error => {
+        console.log(error)
+        return Promise.reject(error)
+    }
+)
 
 export default api
