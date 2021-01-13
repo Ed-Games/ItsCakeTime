d59a42f891caf5f01ee2bf01efb1519a6026dbc4
Ed-Games <edmarcosfilho2015@gmail.com>
2021-01-13 14:59:24 -0300

    update: sending data to create user

--- Mobile/src/pages/Register/Register.tsx
+++ Mobile/src/pages/Register/Register.tsx
@@ -1,18 +1,49 @@
-import React from 'react'
-import { Image, ImageBackground, Text, View, Dimensions } from 'react-native'
+import React, { useState } from 'react'
+import { ImageBackground, Text, View, Dimensions, Alert } from 'react-native'
 import Waves from '../../images/waves.png'
 import styles from './styles'
 import Header from '../../components/Header/Header'
-import { RectButton, ScrollView, TextInput } from 'react-native-gesture-handler'
+import { RectButton, ScrollView} from 'react-native-gesture-handler'
 import Input from '../../components/Input/Input'
 import { useNavigation } from '@react-navigation/native'
+import api from '../../services/api'
 
 export default function Register(){
 
+    const [name, setName] = useState<string>('')
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const[confirmPassword, setConfirmPassword] = useState<string>('')
+    const [whatsapp,setWhatsapp] = useState<string>('')
+    const info = [name,email,password, whatsapp]
+
+
     const navigation = useNavigation()
     
-    function handleNavigateToLogin(){
-        navigation.navigate('Login')
+
+    async function createUser(){
+
+        if(!name || !email || !password || !whatsapp){
+            Alert.alert('Por favor, precisamos que você preencha todos os dados para criarmos a sua conta')
+        } else if(password != confirmPassword){
+            Alert.alert('senha e confirmar senha precisam ser iguais')
+        } else {
+            const data = {
+                userName : name,
+                email : email,
+                password : password,
+                whatsapp:whatsapp
+            }
+    
+    
+            await api.post('users/register', data).then(response => {
+                console.log(response)
+            }).catch(err => console.log(err))
+
+            navigation.navigate('Login')
+        }
+
+        
     }
 
     return(
@@ -35,12 +66,12 @@ export default function Register(){
                        paddingBottom: 80
                    }}
                    >
-                    <Input name="Nome: " placeholder="Seu Nome de usuário" />
-                    <Input name="E-mail: " placeholder="Informe seu e-mail" />
-                    <Input name="Senha: " placeholder="Informe uma boa senha" />
-                    <Input name="Confirmar senha: " placeholder="Repita a senha anterior " />
-                    <Input name="Whatsapp: " placeholder="Seu numero de whatsapp" />
-                    <RectButton onPress={handleNavigateToLogin} style={styles.submitButton}>
+                    <Input setData={setName} name="Nome: " placeholder="Seu Nome de usuário" />
+                    <Input setData={setEmail} name="E-mail: " placeholder="Informe seu e-mail" />
+                    <Input setData={setPassword} name="Senha: " placeholder="Informe uma boa senha" />
+                    <Input setData={setConfirmPassword} name="Confirmar senha: " placeholder="Repita a senha anterior " />
+                    <Input setData={setWhatsapp} name="Whatsapp: " placeholder="Seu numero de whatsapp" />
+                    <RectButton onPress={createUser} style={styles.submitButton}>
                         <Text style={styles.submitButtonText}>Finalizar Cadastro</Text>
                     </RectButton>
                     
