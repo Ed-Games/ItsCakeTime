b981a953e0e6edcc88773e20f9e507741e871073
Ed-Games <edmarcosfilho2015@gmail.com>
2021-01-25 20:09:24 -0300

    fix: error while validation token

--- backend/src/services/authorization.js
+++ backend/src/services/authorization.js
@@ -51,10 +51,8 @@ function generateAccessToken(userName){
 function authenticateToken(request,response, next){
     const authHeader = request.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
-
     if(token==null) return response.sendStatus(401)
-
-    jwt.verify(token,''+process.env.ACCESS_TOKEN, (error,user)=>{
+    jwt.verify(token,''+process.env.ACCESS_TOKEN_SECRET, (error,user)=>{
         if(error) return response.sendStatus(403)
         request.user = user
         User = user
