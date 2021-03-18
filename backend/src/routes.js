c5575fd6b6edc06f54a29e93edeff8e5e7bf98dd
Ed-Games <edmarcosfilho2015@gmail.com>
2021-03-18 14:02:40 -0300

    update: passing user token into route params

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -61,6 +61,6 @@ routes.post('/requestNewPassword', userController.requestNewPassword)
 
 routes.put('/users/resetPassword/:token/', userController.resetPassword)
 
-routes.get('/app/redirect',(request,response)=> {response.redirect(`exp://10.0.0.105:19000/--/CreateNewPasswd/`)})
+routes.get('/app/redirect/:token',(request,response)=> {response.redirect(`exp://10.0.0.105:19000/--/CreateNewPasswd/`)})
 
 module.exports = routes
