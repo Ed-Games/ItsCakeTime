8df6883e0584ba388b73c228fef9a79a81301a86
Ed-Games <edmarcosfilho2015@gmail.com>
2021-04-10 14:27:02 -0300

    create: interfaces file

---
+++ Mobile/src/interfaces/interfaces.tsx
@@ -0,0 +1,22 @@
+interface Product{
+    id: number,
+    name: string,
+    detail: string,
+    category: string,
+    image: string,
+    price: number,
+    user_id: number,
+    whatsapp?: string,
+    email?: string,
+}
+
+interface Profile {
+    id: number,
+    description: string,
+    whatsapp?: string,
+    user_id: number,
+    image: string,
+    specialty: string,
+    userName: string,
+    email?: string,
+}
