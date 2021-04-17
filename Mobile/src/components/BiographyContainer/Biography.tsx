fcfec740f1ad174dde87f2014e12f9969a728ed1
Ed-Games <edmarcosfilho2015@gmail.com>
2021-04-17 14:31:40 -0300

    create: Biography component

---
+++ Mobile/src/components/BiographyContainer/Biography.tsx
@@ -0,0 +1,82 @@
+import { FontAwesome } from '@expo/vector-icons'
+import AsyncStorage from '@react-native-community/async-storage'
+import { useNavigation } from '@react-navigation/native'
+import React from 'react'
+import { Text, View } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+import EmailButton from '../EmailButton/EmailButton'
+import WhatsappButton from '../WhatsappButton/WhatsappButton'
+import styles from './styles'
+
+interface BiographyProps {
+    data: Profile,
+    user: number
+}
+
+
+export default function Biography(props: BiographyProps){
+
+    const navigation = useNavigation()
+
+    function handleNavigateToProfileProducts(){
+        if(AsyncStorage.getItem('@Key:user')){
+            
+            navigation.navigate('ViewYourProducts')
+        }else{
+            navigation.navigate('ProfileProducts')
+        }
+        
+    }
+
+    function handleNavigateToUpdateProfile(){
+        navigation.navigate('UpdateProfile',{Data: props.data})
+    }
+    
+
+    return (
+        <View style={styles.InfoView}>
+            <Text style={styles.TopicText}>Biografia:</Text>
+            <Text style={styles.ContentText}>
+            {props.data?.description}
+            </Text>
+            <Text style={styles.TopicText}>Especialidades:</Text>
+            <Text style={styles.ContentText}>
+            {props.data?.specialty} 
+            </Text>
+            <Text style={styles.TopicText}>Contato:</Text>
+            <View style={styles.ButtonsView}>
+                <WhatsappButton number={props.data?.whatsapp} />
+                <EmailButton address={props.data?.email as string} />
+            </View>
+            <View style={{alignItems:'center', flexDirection: 'row'}}>
+                {console.log(props.data?.user_id)}
+            { props.user ==props.data?.user_id?(
+                <>
+                    <RectButton onPress={handleNavigateToProfileProducts} style={[styles.ListButton, {alignSelf: 'flex-start'}]} >
+                        <View style={styles.FlexRowView}>
+                            <FontAwesome name="birthday-cake" size={24} color='#FFF' style={{marginLeft: 5, marginTop: 5}} />
+                            <Text style={styles.ButtonText}>Lista de produtos</Text>
+                        </View>
+                    </RectButton>
+                    <RectButton onPress={handleNavigateToUpdateProfile} style={[styles.ListButton,{alignSelf:'flex-end', backgroundColor:'#9553A0'}]} >
+                    <View style={styles.FlexRowView}>
+                        <FontAwesome name="edit" size={24} color='#FFF' style={{marginLeft: 5, marginTop: 5}} />
+                        <Text style={styles.ButtonText}>Editar perfil</Text>
+                    </View>
+                    </RectButton>
+                </>
+                
+            ):(
+                <RectButton onPress={handleNavigateToProfileProducts} style={styles.ListButton} >
+                    <View style={styles.FlexRowView}>
+                        <FontAwesome name="birthday-cake" size={24} color='#FFF' style={{marginLeft: 5, marginTop: 5}} />
+                        <Text style={styles.ButtonText}>Lista de produtos</Text>
+                    </View>
+                </RectButton>
+            )}
+            
+            </View>
+        </View>
+    )       
+
+}
---
+++ Mobile/src/components/BiographyContainer/styles.ts
@@ -0,0 +1,60 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    InfoView : {
+        backgroundColor: '#FFF',
+        width: 328,
+        height: 381,
+        borderRadius: 20,
+        marginTop: 10,
+    },
+
+    TopicText :{
+        margin: 14,
+        fontSize: 16,
+        color: '#455A64',
+        width: 160,
+        height:24,
+        lineHeight: 24,
+        fontFamily: 'Poppins_600SemiBold'
+    },
+
+    ContentText: {
+        marginLeft: 14,
+        marginRight: 9,
+        fontSize:12,
+        fontFamily: 'Poppins_300Light'
+    },
+
+    ButtonsView : {
+        flexDirection: 'row',
+        marginTop: -35
+    },
+
+    FlexRowView : {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+
+    ButtonText : {
+        color: '#FFF',
+        fontFamily: 'Poppins_600SemiBold',
+        fontSize:10,
+        marginTop: 10,
+        marginRight: 20,
+        width:102,
+        marginLeft: 10
+
+    },
+
+    ListButton:{
+        backgroundColor: '#F782EC',
+        width: 143,
+        height:36,
+        borderRadius: 10,
+        marginLeft: 13,
+        marginTop: 27,
+    },
+})
+
+export default styles
