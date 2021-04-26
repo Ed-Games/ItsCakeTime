c8f3713f634e7c9571a84fd5108eb2012c2453b4
Ed-Games <edmarcosfilho2015@gmail.com>
2021-04-26 16:08:00 -0300

    create: styles for ViewProfileProducts screen

---
+++ Mobile/src/pages/ViewProfileProducts/styles.ts
@@ -0,0 +1,34 @@
+import { Dimensions, StyleSheet } from "react-native"
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F5F5F5',
+        alignItems: 'center',
+    },
+
+    header: {
+        width: '100%',
+        flexDirection: 'row',
+        height: Dimensions.get('screen').height * 0.1,
+        backgroundColor: '#9553A0'
+    },
+
+    avatar : {
+        width: 50,
+        height: 50,
+        margin: 10
+    },
+
+    title: {
+        fontSize: 15,
+        fontFamily: 'Archivo_600SemiBold',
+        color: '#FFF',
+        maxWidth: Dimensions.get('screen').width * 0.8,
+        textAlign: 'center',
+        margin:20
+    }
+})
+
+export default styles
