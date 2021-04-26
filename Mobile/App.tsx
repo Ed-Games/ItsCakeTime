d74182a8f6ea7b95045ad83d31833d1e53e9c81d
Ed-Games <edmarcosfilho2015@gmail.com>
2021-04-26 16:03:52 -0300

    refactor: removing useless inports

--- Mobile/App.tsx
+++ Mobile/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
 import {Archivo_600SemiBold,Archivo_700Bold, useFonts} from '@expo-google-fonts/archivo'
 import {Poppins_700Bold,Poppins_600SemiBold, Poppins_600SemiBold_Italic, Poppins_500Medium_Italic, Poppins_300Light, Poppins_400Regular, Poppins_500Medium} from '@expo-google-fonts/poppins'
 import Routes from './src/routes';
