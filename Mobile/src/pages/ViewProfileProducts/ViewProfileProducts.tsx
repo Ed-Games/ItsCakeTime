e54cff1583bd7336211569bda0b491f499b63053
Ed-Games <edmarcosfilho2015@gmail.com>
2021-05-01 13:59:08 -0300

    update: created RouteProps interface, getProducts function and useEffect . Calling ProductItem component to show results on screen

--- Mobile/src/pages/ViewProfileProducts/ViewProfileProducts.tsx
+++ Mobile/src/pages/ViewProfileProducts/ViewProfileProducts.tsx
@@ -1,28 +1,57 @@
-import React, { useEffect } from 'react'
-import { Image, ImageBackground, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Image, Text, View } from 'react-native'
 
 import avatar from '../../images/avatar.png'
 
 import styles from './styles'
-import { useNavigation, useRoute } from '@react-navigation/native'
+import {useNavigation} from '@react-navigation/native'
+import Header from '../../components/Header/Header'
+import api from '../../services/api'
+import ProductItem from '../../components/ProductItem/ProductItem'
+import { ScrollView } from 'react-native-gesture-handler'
 
-export default function ViewProfileProducts(){
+interface RouteProps{
+    route : {
+        name:string,
+        params:{
+            id:string
+        }
+    }
+}
 
-    const router = useRoute()
-    const navigation = useNavigation()
+export default function ViewProfileProducts({route}:RouteProps){
+    const [products, setProducts] = useState<Product[]>()
 
+    async function getProducts(id:string){
+        await api.get(`profile/${id}/products`).then(response => {
+            setProducts(response.data)
+            console.log(products)
+        })
+    }
     
     useEffect(() => {
-        navigation.addListener('focus',()=>{
-            console.log(router.params)
-        })
-    }, [navigation])
+        console.log("ViewProfileProducts route : ",route.params.id)
+        getProducts(route.params.id)
+    }, [route.params.id])
 
     return(
         <View style={styles.container}>
+            <Header backgroundColor="#9553A0" />
             <View style={styles.header}>
                 <Image source={avatar} style={styles.avatar} />
                 <Text style={styles.title}> Lista de produtos de Fernanda</Text>
+            </View>
+            <View style={styles.products}>
+                <ScrollView contentContainerStyle={{
+                    alignItems: 'center',
+                    paddingBottom:180,
+                }}>
+                    {products?.map(product => {
+                        return(
+                            <ProductItem Data={product} EditButton={false} InfoButton={false} />
+                        )
+                    })}
+                </ScrollView>
             </View>
             
         </View>
