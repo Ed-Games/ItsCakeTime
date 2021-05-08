8e4c09f26af582c73f8d9bca46d12b8972b4d9ef
Ed-Games <edmarcosfilho2015@gmail.com>
2021-05-08 14:53:56 -0300

    update: change picker inport to use the new picker component

--- Mobile/src/components/ReactPicker/ReactPicker.tsx
+++ Mobile/src/components/ReactPicker/ReactPicker.tsx
@@ -1,4 +1,4 @@
-import { Picker } from '@react-native-community/picker'
+import { Picker } from '@react-native-picker/picker'
 import React from 'react'
 import styles from '../../pages/EditProduct/styles'
 
