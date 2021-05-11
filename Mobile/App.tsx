2c1253fc8264db1561d928e96941e3b445747764
Ed-Games <edmarcosfilho2015@gmail.com>
2021-05-11 14:54:46 -0300

    update: using UserContextProvider in the app

--- Mobile/App.tsx
+++ Mobile/App.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet} from 'react-native';
 import {Archivo_600SemiBold,Archivo_700Bold, useFonts} from '@expo-google-fonts/archivo'
 import {Poppins_700Bold,Poppins_600SemiBold, Poppins_600SemiBold_Italic, Poppins_500Medium_Italic, Poppins_300Light, Poppins_400Regular, Poppins_500Medium} from '@expo-google-fonts/poppins'
 import Routes from './src/routes';
 import AppLoading  from 'expo-app-loading';
+import { UserContextProvider } from './src/Contexts/UserContext';
 
 
 export default function App() {
@@ -28,16 +28,9 @@ export default function App() {
   return (
     <>
     <StatusBar style='dark'/>
-    <Routes />
+    <UserContextProvider>
+      <Routes />
+    </UserContextProvider>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
