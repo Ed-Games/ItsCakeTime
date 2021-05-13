041a15442450dddf824dfe0f1707570086ea20bc
Ed-Games <edmarcosfilho2015@gmail.com>
2021-05-13 13:40:52 -0300

    update: loading profile data if a user is logged and displaying it on the Drawer

--- Mobile/src/components/DrawerContent/DrawerContent.tsx
+++ Mobile/src/components/DrawerContent/DrawerContent.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {Image, Text,View} from 'react-native'
 import {DrawerContentComponentProps, DrawerContentOptions, DrawerContentScrollView, DrawerItem} from '@react-navigation/drawer'
 import styles from './styles'
@@ -7,34 +7,62 @@ import { useNavigation } from '@react-navigation/native'
 import ButtonLink from './ButtonLink'
 import { Feather } from '@expo/vector-icons'
 import Avatar from '../../images/avatar.png'
+import { useUser } from '../../Contexts/UserContext'
 
 export default function DrawerContent(props:DrawerContentComponentProps<DrawerContentOptions>){
 
+    const {LoadProfileDataFromAPI, profileData, loggedUser} = useUser()
     
     function handleNavigateToLogout(){
         props.navigation.navigate('Logout')
     }
 
+    function handleNavigateToLogin(){
+        props.navigation.navigate('Login')
+    }
+
+    useEffect(()=>{
+        LoadProfileDataFromAPI()
+        console.log("Drawer request: ",profileData)
+    },[loggedUser])
 
     return(
         <View style={styles.container}>
-            <View style={styles.ImageView}>
-                <Image source={Avatar} style={styles.Avatar} />
-                <Text style={styles.AvatarNameText}>Alice Andrade Campus</Text>
-            </View>
+            {profileData? (
+                <View style={styles.ImageView}>
+                    <Image source={{uri: profileData.imageUrl}} style={styles.Avatar} />
+                    <Text style={styles.AvatarNameText}>{profileData.userName}</Text>
+                </View>
+            ) : (
+                <View style={styles.ImageView}>
+                    <Image source={Avatar} style={styles.Avatar} />
+                    <Text style={styles.AvatarNameText}>Convidado</Text>
+                </View>
+            )}
             <View style={{marginTop:94, alignItems: 'center'}}>
                 <View style={styles.LineSpace}></View>
                 <ButtonLink icon="home" title="Inicio" drawerProps={props} navigateTO='Landing' />
-                <ButtonLink icon="user" title="Seu Perfil" drawerProps={props} navigateTO='Profile' />
+                {profileData && (
+                    <ButtonLink icon="user" title="Seu Perfil" drawerProps={props} navigateTO='Profile' />
+                )}
                 <ButtonLink icon="shopping-cart" title="Lista de Produtos" drawerProps={props} navigateTO='ProductList' />
                 <ButtonLink icon="search" title="Pesquisar Padeiros" drawerProps={props} navigateTO='Search Profiles' />
-                <ButtonLink icon="plus-circle" title="Novo Produto" drawerProps={props} navigateTO='ProductRegister' />
+                {profileData && (
+                    <ButtonLink icon="plus-circle" title="Novo Produto" drawerProps={props} navigateTO='ProductRegister' />
+                )}
             </View>
             <View style={styles.BottomContainer}>
-                <RectButton onPress={handleNavigateToLogout} style={styles.SignOutButton}>
-                    <Feather name="log-out" size={24} color='#9553A0'/>
-                    <Text style={styles.SignOutText}>sair</Text>
-                </RectButton>
+                {profileData?(
+                    <RectButton onPress={handleNavigateToLogout} style={styles.SignOutButton}>
+                        <Feather name="log-out" size={24} color='#9553A0'/>
+                        <Text style={styles.SignOutText}>sair</Text>
+                    </RectButton>
+                ):(
+                    <RectButton onPress={handleNavigateToLogin} style={styles.SignOutButton}>
+                        <Feather name="log-in" size={24} color='#9553A0'/>
+                        <Text style={styles.SignOutText}>Entrar</Text>
+                    </RectButton>
+                )}
             </View>
         </View>
     )
