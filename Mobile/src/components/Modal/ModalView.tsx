63d4c9ad3d0a6e897aa588049b1f02101bb60a2e
Ed-Games <edmarcosfilho2015@gmail.com>
2021-05-24 21:02:32 -0300

    fix: change type of Modal Component Children to ReactNode

--- Mobile/src/components/Modal/ModalView.tsx
+++ Mobile/src/components/Modal/ModalView.tsx
@@ -1,11 +1,11 @@
 import { useNavigation } from '@react-navigation/native'
-import React, { useEffect, useState } from 'react'
+import React, { ReactNode, useEffect, useState } from 'react'
 import { View } from 'react-native'
 import {Provider,Modal, Text, Portal, Button} from 'react-native-paper'
 import styles from './ModalViewStyles'
 
 interface ModalProps{
-    children: JSX.Element,
+    children: ReactNode,
     isVisible: boolean,
     title: string,
     setStateFunction: (value: React.SetStateAction<boolean>) => void
