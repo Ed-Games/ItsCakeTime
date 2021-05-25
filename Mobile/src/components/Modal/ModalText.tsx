0b39cdbd36e4c551863ea88bc0a282de59b1f84a
Ed-Games <edmarcosfilho2015@gmail.com>
2021-05-24 21:02:03 -0300

    update: passing styles by props on ModalText Component

--- Mobile/src/components/Modal/ModalText.tsx
+++ Mobile/src/components/Modal/ModalText.tsx
@@ -3,12 +3,19 @@ import {Text} from 'react-native'
 import styles from './ModalViewStyles'
 
 interface TextProps{
-    children:string
+    children:string,
+    style?: object
 }
 
 export function ModalText(props: TextProps){
+    const style = {
+        ...styles.ModalText,
+        ...props.style,
+    }
+
+    console.log(style)
     return(
-        <Text style={styles.ModalText}>
+        <Text style={style}>
             {props.children}
         </Text>
     )
