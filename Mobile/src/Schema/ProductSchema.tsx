3cfdca5504aac5535f91b40bf9b9aa79331bcf43
Ed-Games <edmarcosfilho2015@gmail.com>
2021-06-01 13:49:47 -0300

    update: added image to schema

--- Mobile/src/Schema/ProductSchema.tsx
+++ Mobile/src/Schema/ProductSchema.tsx
@@ -4,5 +4,6 @@ export const ProductSchema = yup.object().shape({
     name: yup.string().required("Um nome é necessário"),
     details: yup.string().required("Adicione uma descrição"),
     category: yup.string(),
-    price: yup.string().required("seu produto precisa ter um preço")
+    price: yup.string().required("seu produto precisa ter um preço"),
+    image: yup.string().required('Uma imagem é necessária')
 })
