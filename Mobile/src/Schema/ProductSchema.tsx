bd235b308ad9b844c4bdacdb0ef3f83ab1e4e7c5
Ed-Games <edmarcosfilho2015@gmail.com>
2021-06-12 13:24:42 -0300

    update: category now is required

--- Mobile/src/Schema/ProductSchema.tsx
+++ Mobile/src/Schema/ProductSchema.tsx
@@ -3,7 +3,7 @@ import * as yup from 'yup'
 export const ProductSchema = yup.object().shape({
     name: yup.string().required("Um nome é necessário"),
     details: yup.string().required("Adicione uma descrição"),
-    category: yup.string(),
+    category: yup.string().required("Adicione uma categoria"),
     price: yup.string().required("seu produto precisa ter um preço"),
     image: yup.string().required('Uma imagem é necessária')
 })
