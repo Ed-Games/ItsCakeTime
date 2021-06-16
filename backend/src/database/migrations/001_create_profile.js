42226271fd6f2e671a9d40086b84fdb205673aff
Ed-Games <edmarcosfilho2015@gmail.com>
2021-06-16 14:09:25 -0300

    update: description and specialty no longer have a default value

--- backend/src/database/migrations/001_create_profile.js
+++ backend/src/database/migrations/001_create_profile.js
@@ -1,11 +1,11 @@
 exports.up = function(Knex){
     return Knex.schema.createTable('profile', function(table){
         table.increments();
-        table.string('description').defaultTo("Nenhuma biografia adicionada");
+        table.string('description');
         table.string('whatsapp').notNullable();
         table.decimal('user_id').notNullable();
-        table.string('image');
-        table.string('specialty').defaultTo("Nenhuma especialidade informada");
+        table.string('image').notNullable();
+        table.string('specialty');
         table.unique('user_id');
 
         table.foreign('user_id').references('id').inTable('user').onDelete('CASCADE');
