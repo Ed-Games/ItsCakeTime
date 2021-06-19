124a8fc09fa481f99741edf6257d8a73ec733e92
Ed-Games <edmarcosfilho2015@gmail.com>
2021-06-18 22:39:15 -0300

    create: Profile Schema

---
+++ Mobile/src/Schema/ProfileSchema.ts
@@ -0,0 +1,7 @@
+import * as yup from 'yup'
+
+export const ProfileSchema = yup.object().shape({
+    description: yup.string().max(500,'Limite de  500 caracteres'),
+    specialty: yup.string().max(100, 'Limite de 100 caracteres'),
+    whatsapp: yup.string().min(11, 'minimo de 11 digitos').max(13, 'máximo de 13 dígitos').required("Este campo é obrigatório"),
+})
