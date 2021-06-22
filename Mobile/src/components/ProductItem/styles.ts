1bb8cf343603d26a8e3d1b6166b6c6983c407925
Ed-Games <edmarcosfilho2015@gmail.com>
2021-06-22 15:01:20 -0300

    update: resized productItem card

--- Mobile/src/components/ProductItem/styles.ts
+++ Mobile/src/components/ProductItem/styles.ts
@@ -59,7 +59,7 @@ const styles = StyleSheet.create({
         width: 220,
         fontFamily: 'Poppins_300Light',
         fontSize: 15,
-        height: 120
+        height: 80
     },
 
     LinkButton:{
