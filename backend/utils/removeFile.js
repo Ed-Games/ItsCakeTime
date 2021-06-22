8f6370b1090b3c70653d79b7569b3810890c63c8
Ed-Games <edmarcosfilho2015@gmail.com>
2021-06-22 13:28:41 -0300

    fix: file path

--- backend/utils/removeFile.js
+++ backend/utils/removeFile.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const path = require('path')
 
 async function RemoveFile(file){
-    const fp = path.resolve(__dirname,'..','..','uploads', file)
-    console.log('path: ', fp)
+    const fp = path.resolve(__dirname,'..','uploads', file)
+    console.log(__dirname)
     fs.unlink(fp, (err) => {
         if (err) {
           console.error(err)
