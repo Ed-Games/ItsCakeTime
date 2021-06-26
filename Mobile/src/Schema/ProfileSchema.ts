2e270da5b0a711a1e0a71bb21829e8e2450acb6e
Ed-Games <edmarcosfilho2015@gmail.com>
2021-06-26 14:26:50 -0300

    fix: max length for number is 15 digits

--- Mobile/src/Schema/ProfileSchema.ts
+++ Mobile/src/Schema/ProfileSchema.ts
@@ -3,5 +3,5 @@ import * as yup from 'yup'
 export const ProfileSchema = yup.object().shape({
     description: yup.string().max(500,'Limite de  500 caracteres'),
     specialty: yup.string().max(100, 'Limite de 100 caracteres'),
-    whatsapp: yup.string().min(11, 'minimo de 11 digitos').max(13, 'máximo de 13 dígitos').required("Este campo é obrigatório"),
+    whatsapp: yup.string().min(11, 'minimo de 11 digitos').max(15, 'máximo de 15 dígitos').required("Este campo é obrigatório"),
 })
--- Mobile/src/Schema/RegisterSchema.tsx
+++ Mobile/src/Schema/RegisterSchema.tsx
@@ -5,5 +5,5 @@ export const Registervalidation = yup.object().shape({
     email:yup.string().email().required("Um email é obrigatório").trim(),
     password: yup.string().required("Senha é obrigatória"),
     confirmPassword: yup.string().oneOf([yup.ref('password'), null], "As senhas não combinam").required("obrigatório"),
-    whatsapp:yup.string().required("Um número de Whatsapp é necessário")
+    whatsapp: yup.string().min(11, 'minimo de 11 digitos').max(15, 'máximo de 15 dígitos').required("Este campo é obrigatório"), 
 })
