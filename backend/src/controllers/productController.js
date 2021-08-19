204a49ba5d002ae488bbad4de2f1231902c96bb9
Ed-Games <edmarcosfilho2015@gmail.com>
2021-08-19 20:34:22 -0300

    update: fixing imageUrl prefix

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -1,6 +1,6 @@
 const connection = require('../database/connection')
 const ip = require('ip')
-const prefix = process.env.PROJECT_MODE == 'production'? 'itscaketime-server.herokuapp.com': `${ip.address()}:3333/uploads`
+const prefix = process.env.PROJECT_MODE == 'production'? 'itscaketime-server.herokuapp.com/uploads': `${ip.address()}:3333/uploads`
 const { RemoveFile } = require('../../utils/removeFile')
 
 module.exports = {
