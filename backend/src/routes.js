93e0d79dc3b923e1e1920a3983dc16a4905bef14
Ed-Games <edmarcosfilho2015@gmail.com>
2021-08-18 21:10:09 -0300

    .

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,7 +8,7 @@ const upload = require('./services/multerConfig')
 
 const routes = express.Router()
 const { authenticateToken, refreshToken } = require('./services/authorization')
-const appUrl = process.env.PROJECT_MODE == 'production'? 'itscaketime:///CreateNewPasswd/' : 'exp://10.0.0.105:19000/--/CreateNewPasswd/'
+const appUrl = process.env.PROJECT_MODE? 'itscaketime:///CreateNewPasswd/' : 'exp://10.0.0.105:19000/--/CreateNewPasswd/'
 
 routes.use(express.json())
 
