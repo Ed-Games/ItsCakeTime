b71aa84df52d557ddb7325a8e0a2b1197500323c
Ed-Games <edmarcosfilho2015@gmail.com>
2021-09-07 22:22:12 -0300

    create: default schema exporter

---
+++ backend/src/schemas/schemas.js
@@ -0,0 +1,9 @@
+const loginSchema = require("./loginSchema");
+const userSchema = require("./userSchema");
+const profileSchema = require("./profileSchema")
+
+module.exports = {
+    loginSchema,
+    userSchema,
+    profileSchema
+}
