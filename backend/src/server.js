acd132f03a0b92a317a0125f4e44ba327881e01f
Ed-Games <edmarcosfilho2015@gmail.com>
2023-06-03 10:11:24 -0300

    update: added morgan for requests logging

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,12 @@ const routes = require('./routes')
 const cors = require('cors');
 const path = require('path')
 const fs = require('fs')
+const logger = require('morgan');
 
 const uploads = path.resolve(__dirname,'..','uploads')
 const app = express()
+
+app.use(logger('dev'))
 
 app.use(cors());
 
