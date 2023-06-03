9096cf73eb0a99d4952e0e7783c597f3bbc3db69
Ed-Games <edmarcosfilho2015@gmail.com>
2023-06-02 22:06:43 -0300

    refactor: using undefined instead of null as default value

--- Mobile/src/Schema/RegisterSchema.ts
+++ Mobile/src/Schema/RegisterSchema.ts
@@ -4,6 +4,6 @@ export const Registervalidation = yup.object().shape({
     name: yup.string().required("Nome de usuário é obrigatório").trim(),
     email:yup.string().email().required("Um email é obrigatório").trim(),
     password: yup.string().required("Senha é obrigatória"),
-    confirmPassword: yup.string().oneOf([yup.ref('password'), null], "As senhas não combinam").required("obrigatório"),
+    confirmPassword: yup.string().oneOf([yup.ref('password'), undefined], "As senhas não combinam").required("obrigatório"),
     whatsapp: yup.string().min(11, 'minimo de 11 digitos').max(15, 'máximo de 15 dígitos').required("Este campo é obrigatório"), 
 })
--- Mobile/src/Schema/passwordValidationSchema.ts
+++ Mobile/src/Schema/passwordValidationSchema.ts
@@ -2,5 +2,5 @@ import * as yup from 'yup'
 
 export const passwordValidationSchema = yup.object().shape({
     password: yup.string().required("Senha é obrigatória"),
-    confirmPassword: yup.string().oneOf([yup.ref('password'), null], "As senhas não combinam").required("obrigatório"),
+    confirmPassword: yup.string().oneOf([yup.ref('password'), undefined], "As senhas não combinam").required("obrigatório"),
 })
