3bb8116f9bdb57ed39542494c254353806007f77
Ed-Games <edmarcosfilho2015@gmail.com>
2023-06-02 23:09:30 -0300

    refactor: code reformated

--- Mobile/src/routes.tsx
+++ Mobile/src/routes.tsx
@@ -1,75 +1,77 @@
-import React from 'react'
-import {NavigationContainer} from '@react-navigation/native'
-import { createDrawerNavigator} from '@react-navigation/drawer'
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
+import Landing from "./pages/Landing/Landing";
+import SearchScreen from "./components/Screens/SearchScreen";
 
-import Landing from './pages/Landing/Landing'
-import SearchScreen from './components/Screens/SearchScreen'
+import DrawerContent from "./components/DrawerContent/DrawerContent";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
+import Logout from "./pages/Logout/Logout";
+import ResetPasswd from "./pages/ResetPasswd/ResetPasswd";
+import CreateNewPasswd from "./pages/CreateNewPasswd/CreateNewPasswd";
+import UpdateProfile from "./pages/UpdateProfile/UpdateProfile";
+import ProductRegister from "./pages/ProductRegister/ProductRegister";
+import ViewYourProducts from "./pages/ViewYourProducts/ViewYourProducts";
+import EditProduct from "./pages/EditProduct/EditProduct";
+import * as Linking from "expo-linking";
+import Profile from "./pages/Profile/Profile";
+import ViewProfileProducts from "./pages/ViewProfileProducts/ViewProfileProducts";
+import ProductsList from "./pages/ProductsList/ProductsList";
+import { navigationRef } from "./services/RootNavigation";
 
-import DrawerContent from './components/DrawerContent/DrawerContent'
-import Register from './pages/Register/Register'
-import Login from './pages/Login/Login'
-import Logout from './pages/Logout/Logout'
-import ResetPasswd from './pages/ResetPasswd/ResetPasswd'
-import CreateNewPasswd from './pages/CreateNewPasswd/CreateNewPasswd'
-import UpdateProfile from './pages/UpdateProfile/UpdateProfile'
-import ProductRegister from './pages/ProductRegister/ProductRegister'
-import ViewYourProducts from './pages/ViewYourProducts/ViewYourProducts'
-import EditProduct from './pages/EditProduct/EditProduct'
-import * as Linking from 'expo-linking'
-import Profile from './pages/Profile/Profile'
-import ViewProfileProducts from './pages/ViewProfileProducts/ViewProfileProducts'
-import ProductsList from './pages/ProductsList/ProductsList'
-import {navigationRef} from './services/RootNavigation'
-
-const Drawer = createDrawerNavigator()
-const prefix = Linking.createURL('/');
-
-
+const Drawer = createDrawerNavigator();
+const prefix = Linking.createURL("/");
 
 export default function Routes() {
+  const config = {
+    screens: {
+      CreateNewPasswd: {
+        path: "CreateNewPasswd/:token/:email",
+        parse: {
+          token: (token: string) => `${token}`,
+          email: (email: string) => `${email}`,
+        },
 
-    const config= {
-        screens :{
-            CreateNewPasswd :{
-                path: 'CreateNewPasswd/:token/:email',
-                parse: {
-                    token: (token : string) =>  `${token}`,
-                    email: (email : string) =>  `${email}`,
-                },
+        stringify: {
+          token: (token: string) => token.replace(/^CreateNewPasswd/, ""),
+          email: (email: string) => email.replace(/^CreateNewPasswd/, ""),
+        },
+      },
+    },
+  };
 
-                stringify:{
-                    token: (token : string) => token.replace(/^CreateNewPasswd/,''),
-                    email: (email : string) => email.replace(/^CreateNewPasswd/,'')
-                }
-            }
-        }
-    }
+  const linking = {
+    prefixes: [prefix],
+    config,
+  };
 
-    const linking = {
-        prefixes: [prefix],
-        config
-      }
-
-    return(
-        <NavigationContainer ref={navigationRef} linking={linking}>
-            <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}  initialRouteName="Landing">
-                <Drawer.Screen name="Landing" component={Landing} />
-                <Drawer.Screen name="ProductList" component={ProductsList} />
-                <Drawer.Screen name="Profile" component={Profile} />
-                <Drawer.Screen name="Details" component={Profile} />
-                <Drawer.Screen name="ViewProfileProducts" component={ViewProfileProducts} />
-                <Drawer.Screen name="Search Profiles" component={SearchScreen} />
-                <Drawer.Screen name="Register" component={Register} />
-                <Drawer.Screen name="Login" component={Login} />
-                <Drawer.Screen name="Logout" component={Logout} />
-                <Drawer.Screen name="ResetPasswd" component={ResetPasswd} />
-                <Drawer.Screen name="CreateNewPasswd" component={CreateNewPasswd} />
-                <Drawer.Screen name="UpdateProfile" component={UpdateProfile} />
-                <Drawer.Screen name="ViewYourProducts" component={ViewYourProducts} />
-                <Drawer.Screen name="ProductRegister" component={ProductRegister} />
-                <Drawer.Screen name="EditProduct" component={EditProduct} />
-            </Drawer.Navigator>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer ref={navigationRef} linking={linking}>
+      <Drawer.Navigator
+        drawerContent={(props) => <DrawerContent {...props} />}
+        initialRouteName="Landing"
+      >
+        <Drawer.Screen name="Landing" component={Landing} />
+        <Drawer.Screen name="ProductList" component={ProductsList} />
+        <Drawer.Screen name="Profile" component={Profile} />
+        <Drawer.Screen name="Details" component={Profile} />
+        <Drawer.Screen
+          name="ViewProfileProducts"
+          component={ViewProfileProducts}
+        />
+        <Drawer.Screen name="Search Profiles" component={SearchScreen} />
+        <Drawer.Screen name="Register" component={Register} />
+        <Drawer.Screen name="Login" component={Login} />
+        <Drawer.Screen name="Logout" component={Logout} />
+        <Drawer.Screen name="ResetPasswd" component={ResetPasswd} />
+        <Drawer.Screen name="CreateNewPasswd" component={CreateNewPasswd} />
+        <Drawer.Screen name="UpdateProfile" component={UpdateProfile} />
+        <Drawer.Screen name="ViewYourProducts" component={ViewYourProducts} />
+        <Drawer.Screen name="ProductRegister" component={ProductRegister} />
+        <Drawer.Screen name="EditProduct" component={EditProduct} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
