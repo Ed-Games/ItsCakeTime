f7da4b5747dfa5a5a1f00188aba7ee686db4b8ce
Ed-Games <edmarcosfilho2015@gmail.com>
2023-06-04 15:32:18 -0300

    style: code reformated

--- Mobile/src/components/DrawerContent/ButtonLink.tsx
+++ Mobile/src/components/DrawerContent/ButtonLink.tsx
@@ -1,30 +1,31 @@
-import { Feather } from '@expo/vector-icons'
-import { DrawerContentComponentProps, DrawerContentOptions } from '@react-navigation/drawer'
-import React from 'react'
-import { Text, View } from 'react-native'
-import { RectButton } from 'react-native-gesture-handler'
-import styles from './styles'
+import { Feather } from "@expo/vector-icons";
+import { DrawerContentComponentProps } from "@react-navigation/drawer";
+import React from "react";
+import { Text, View } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+import styles from "./styles";
 
-interface ButtonLinkProps{
-    drawerProps: DrawerContentComponentProps<DrawerContentOptions>,
-    title: String,
-    navigateTO: string,
-    icon: string
+interface ButtonLinkProps {
+  drawerProps: DrawerContentComponentProps;
+  title: String;
+  navigateTO: string;
+  icon: string;
 }
 
-export default function ButtonLink(props: ButtonLinkProps){
-
-    function handleNavigateToLandingPage(){
-        props.drawerProps.navigation.navigate(props.navigateTO)
-    }
-    return(
-        <>
-            <RectButton onPress={handleNavigateToLandingPage} style={styles.ButtonLink}>
-                <Feather name={props.icon} size={24} color='#9553A0'/>    
-                <Text style={styles.LinkText}>{props.title}</Text>
-            </RectButton>
-            <View style={styles.LineSpace}>
-            </View>
-        </>
-    )
+export default function ButtonLink(props: ButtonLinkProps) {
+  function handleNavigateToLandingPage() {
+    props.drawerProps.navigation.navigate(props.navigateTO);
+  }
+  return (
+    <>
+      <RectButton
+        onPress={handleNavigateToLandingPage}
+        style={styles.ButtonLink}
+      >
+        <Feather name={props.icon as any} size={24} color="#9553A0" />
+        <Text style={styles.LinkText}>{props.title}</Text>
+      </RectButton>
+      <View style={styles.LineSpace}></View>
+    </>
+  );
 }
