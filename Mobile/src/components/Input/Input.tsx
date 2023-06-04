e0777069434335880112a38196990a80ba4bcbba
Ed-Games <edmarcosfilho2015@gmail.com>
2023-06-04 15:39:22 -0300

    style: code reformatted

--- Mobile/src/components/Input/Input.tsx
+++ Mobile/src/components/Input/Input.tsx
@@ -1,45 +1,46 @@
-import React, { ChangeEvent } from 'react'
-import {Text, TextInput, View } from 'react-native'
-import styles from './styles'
+import React, { ChangeEvent } from "react";
+import { Text, TextInput, View } from "react-native";
+import styles from "./styles";
 
-interface InputProps{
-    name: string,
-    placeholder?: string,
-    options?: {
-        titleMode?: string,
-        customStyle?: object,
-        useAsTextArea?: boolean,
-        TextInputStyle?: object
-    },
+interface InputProps {
+  name: string;
+  placeholder?: string;
+  options?: {
+    titleMode?: string;
+    customStyle?: object;
+    useAsTextArea?: boolean;
+    TextInputStyle?: object;
+  };
 
-    defaultValue?: string,
-    secureTextEntry?: boolean,
-    value?:string,
-    setData?:  React.Dispatch<React.SetStateAction<string>>|((e: string | ChangeEvent<any>) => void),
-    captalize?: any
+  defaultValue?: string;
+  secureTextEntry?: boolean;
+  value?: string;
+  setData?:
+    | React.Dispatch<React.SetStateAction<string>>
+    | ((e: string | ChangeEvent<any>) => void);
+  captalize?: any;
 }
 
-
-export default function Input(props: InputProps){
-    return(
-        <>
-            {props.options?.titleMode =='Light'? 
-            (
-                <Text style={[styles.InputText,{color:"#FFF"}]}>{props.name}</Text>
-            ):(
-                <Text style={styles.InputText}>{props.name}</Text>
-            )}
-                <View style={[styles.FormInputView,props.options?.customStyle]}>
-                    <TextInput autoCapitalize={props.captalize} 
-                    secureTextEntry={props.secureTextEntry}
-                    onChangeText={props.setData} 
-                    value={props.value} 
-                    defaultValue={props?.defaultValue} 
-                    multiline={props.options?.useAsTextArea} 
-                    style={[styles.Input,props.options?.TextInputStyle]} 
-                    placeholder={props.placeholder}>
-                    </TextInput>
-                </View>
-        </>
-    )
+export default function Input(props: InputProps) {
+  return (
+    <>
+      {props.options?.titleMode == "Light" ? (
+        <Text style={[styles.InputText, { color: "#FFF" }]}>{props.name}</Text>
+      ) : (
+        <Text style={styles.InputText}>{props.name}</Text>
+      )}
+      <View style={[styles.FormInputView, props.options?.customStyle]}>
+        <TextInput
+          autoCapitalize={props.captalize}
+          secureTextEntry={props.secureTextEntry}
+          onChangeText={props.setData}
+          value={props.value}
+          defaultValue={props?.defaultValue}
+          multiline={props.options?.useAsTextArea}
+          style={[styles.Input, props.options?.TextInputStyle]}
+          placeholder={props.placeholder}
+        ></TextInput>
+      </View>
+    </>
+  );
 }
