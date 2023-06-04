c05afba49d21270ba2c53e392b5698210a202c5d
Ed-Games <edmarcosfilho2015@gmail.com>
2023-06-04 15:34:00 -0300

    styles: code reformated

--- Mobile/src/components/EmailButton/EmailButton.tsx
+++ Mobile/src/components/EmailButton/EmailButton.tsx
@@ -1,30 +1,33 @@
-import { Feather } from '@expo/vector-icons'
-import React from 'react'
-import { Text, View } from 'react-native'
-import { RectButton } from 'react-native-gesture-handler'
-import styles from './styles'
-import * as MailComposer from 'expo-mail-composer'
+import { Feather } from "@expo/vector-icons";
+import React from "react";
+import { Text, View } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+import styles from "./styles";
+import * as MailComposer from "expo-mail-composer";
 
 interface EmailProps {
-    address: string
+  address: string;
 }
 
 export default function EmailButton(props: EmailProps) {
+  function SendEmail() {
+    MailComposer.composeAsync({
+      subject: "Encomenda do It'cakeTime ",
+      recipients: [props.address],
+    });
+  }
 
-    function SendEmail(){
-        MailComposer.composeAsync({
-            subject:"Encomenda do It'cakeTime ",
-            recipients:[props.address],
-            
-        })
-    }
-
-    return(
-        <RectButton onPress={SendEmail} style={styles.EmailButton}>
-            <View style={styles.FlexRowView}>
-                <Feather name="mail" size={24} color='#FFF' style={{marginLeft: 5, marginTop: 5}} />
-                <Text style={styles.ButtonText}>Enviar um Email</Text>
-            </View>
-        </RectButton>
-    )
+  return (
+    <RectButton onPress={SendEmail} style={styles.EmailButton}>
+      <View style={styles.FlexRowView}>
+        <Feather
+          name="mail"
+          size={24}
+          color="#FFF"
+          style={{ marginLeft: 5, marginTop: 5 }}
+        />
+        <Text style={styles.ButtonText}>Enviar um Email</Text>
+      </View>
+    </RectButton>
+  );
 }
