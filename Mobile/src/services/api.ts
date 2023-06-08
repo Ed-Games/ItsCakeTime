6178a6fc12e36cac0ce9ec76c9098d353eedd4a2
Ed-Games <edmarcosfilho2015@gmail.com>
2023-06-08 15:32:59 -0300

    fix: replaced "Key:user" with "user" as the key to get the user on the storage

--- Mobile/src/services/api.ts
+++ Mobile/src/services/api.ts
@@ -1,64 +1,62 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import axios from 'axios'
-import {Navigate} from './RootNavigation'
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import { Navigate } from "./RootNavigation";
 
-import {APP_URL } from '@env';
+import { APP_URL } from "@env";
 
-async function GetUserData(){
-    const userData = await AsyncStorage.getItem('@Key:user')
-    if(userData) return JSON.parse(userData)
+async function GetUserData() {
+  const userData = await AsyncStorage.getItem("user");
+  if (userData) return JSON.parse(userData);
 }
 
 const api = axios.create({
-    baseURL: APP_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    }
-})
+  baseURL: APP_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
 api.interceptors.response.use(
-    response => {
-        return response
-    },
+  (response) => {
+    return response;
+  },
 
-    error => {
-        if(error.message == 'Network Error'){
-            Navigate('Landing',{message:'connection error'})
-            return
-            
-        }
+  (error) => {
+    if (error.message == "Network Error") {
+      Navigate("Landing", { message: "connection error" });
+      return;
+    }
 
-        if(error.response.status===401|| error.response.status===403){
-            const requestConfig = error.config
-            Navigate('Login',{})
-        
-            return axios(requestConfig)
-        }
+    if (error.response.status === 401 || error.response.status === 403) {
+      const requestConfig = error.config;
+      Navigate("Login", {});
 
-        return Promise.reject(error)
-    },
-          
+      return axios(requestConfig);
+    }
 
-)
+    return Promise.reject(error);
+  }
+);
 
 api.interceptors.request.use(
-    config => {
-        return GetUserData().then(user =>{
-            if(user && user.accessToken)
-                config.headers.authorization = `Bearer ${user.accessToken}` 
-            return Promise.resolve(config)
+  (config) => {
+    return GetUserData()
+      .then((user) => {
+        if (user && user.accessToken) {
+          config.headers.authorization = `Bearer ${user.accessToken}`;
+        }
+        return Promise.resolve(config);
+      })
+      .catch((err) => {
+        console.log(err);
 
-        }).catch(err =>{
-            console.log(err)
+        return Promise.resolve(config);
+      });
+  },
 
-            return Promise.resolve(config)
-        } )
-    },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
-    error => {
-        return Promise.reject(error)
-    }
-)
-
-
-export default api
+export default api;
--- Mobile/src/utils/DeleteUser.ts
+++ Mobile/src/utils/DeleteUser.ts
@@ -1,6 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default async function DeleteUser(){
-    const user = AsyncStorage.removeItem('@Key:user')
+    const user = AsyncStorage.removeItem('user')
     if(user) return user
 }
